a85283ee880067301ab037f8c83cbac4940602d6
Ilya <ilya@1-tn.com>
2023-12-10 23:28:06 +0100

    partially added cardService

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', function(req, res) {
   res.render('pages/main');
