b7bf7b6ee0c03ec873b2d111dcfe2fc93c106166
Ilya <ilya@1-tn.com>
2023-12-10 21:13:22 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('pages/main');
+})
+
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
